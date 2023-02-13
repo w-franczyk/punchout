016672fe163ce3d14db01fb55b8b2c033c69b5a9
Wojciech Franczyk <wojciech.franczyk@posteo.net>
2023-02-13 20:37:02 +0000

    the tasks list's rows are working now and starting to look better

--- overseer/overseer.py
+++ overseer/overseer.py
@@ -1,31 +1,73 @@
 #!/usr/bin/python
 
 from kivy.app import App
+from kivy.properties import BooleanProperty
+from kivy.properties import ListProperty
+from kivy.properties import NumericProperty
+from kivy.properties import StringProperty
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.behaviors import FocusBehavior
+from kivy.uix.label import Label
+from kivy.uix.recycleboxlayout import RecycleBoxLayout
+from kivy.uix.recycleview import RecycleView
+from kivy.uix.recycleview.layout import LayoutSelectionBehavior
+from kivy.uix.recycleview.views import RecycleDataViewBehavior
 from kivy.uix.widget import Widget
 
 class Overseer(Widget):
-    pass
+    def onReleaseBtnNewCategory(self):
+        self.ids.btnNewTask.text = "new category"
+        self.ids.rvTasks.data.insert(0, {'dupa': 'dupsko'})
 
 class OverseerApp(App):
     def build(self):
         return Overseer()
+class ColorLabel(Label):
+    bgColor = ListProperty([0.0, 0.0, 0.0, 1])
 
-class BtnExport(Button):
-    def on_release(self):
-        print("export")
+class TasksRecycleView(RecycleView):
+    def __init__(self, **kwargs):
+        super(TasksRecycleView, self).__init__(**kwargs)
+        self.data = [{'dataId': str(x), 'dataTitle': 'title {}'.format(x)} for x in range(100)]
+        #self.data = [{'text': str(x)} for x in range(100)]
 
-class BtnPrint(Button):
-    def on_release(self):
-        print("print selected")
+class TasksRecycleViewLayout(FocusBehavior, LayoutSelectionBehavior, RecycleBoxLayout):
+    pass
 
-class BtnNewTask(Button):
-    def on_release(self):
-        print("new task")
+class TasksRecycleViewRow(RecycleDataViewBehavior, BoxLayout):
+    ''' Add selection support to the Label '''
+    index = None
+    selected = BooleanProperty(False)
+    selectable = BooleanProperty(True)
+    bgColor = ListProperty([0.09, 0.09, 0.09, 1])
+    borderColor = ListProperty([0.11, 0.11, 0.11, 1])
 
-class BtnNewCategory(Button):
-    def on_release(self):
-        print("new category")
+    # data fields
+    dataId = StringProperty()
+    dataTitle = StringProperty()
+
+    def refresh_view_attrs(self, rv, index, data):
+        ''' Catch and handle the view changes '''
+        self.index = index
+        return super(TasksRecycleViewRow, self).refresh_view_attrs(
+            rv, index, data)
+
+    def on_touch_down(self, touch):
+        ''' Add selection on touch down '''
+        if super(TasksRecycleViewRow, self).on_touch_down(touch):
+            return True
+        if self.collide_point(*touch.pos) and self.selectable:
+            return self.parent.select_with_touch(self.index, touch)
+
+    def apply_selection(self, rv, index, is_selected):
+        ''' Respond to the selection of items in the view. '''
+        self.selected = is_selected
+        #rv.data.insert(0, 'dupa')
+        #if is_selected:
+        #    print("selection changed to {0}".format(rv.data[index]))
+        #else:
+        #    print("selection removed for {0}".format(rv.data[index]))
 
 if __name__ == '__main__':
     OverseerApp().run()
